58fcbefdbb613e184a79c240dc0643dc821b89fe
LIUYang <hyangl@163.com>
2021-03-02 02:03:35 +0800

    template pallet: add do_something weight.

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -67,7 +67,11 @@ decl_module! {
 
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
+		/// # <weight>
+		/// - Base Weight: 44.66 us
+		/// - DB Weight: 1 Write
+		/// # </weight>
+		#[weight = 44_660 + T::DbWeight::get().writes(1)]
 		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
